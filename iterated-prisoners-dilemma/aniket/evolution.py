60952d49518be6b0ef8abaf06bb0731601f0e23e
Aniket <aniketsingh.sachan@gmail.com>
2018-10-24 20:15:19 +0530

    minor changes in code and debug messages added

--- iterated-prisoners-dilemma/aniket/evolution.py
+++ iterated-prisoners-dilemma/aniket/evolution.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 def assign_score(bit_a, bit_b):
     """assign score corresponding to the set of moves
@@ -28,7 +29,7 @@ def play_game(strategy_a, strategy_b):
     score_a = score_b = 0
     encoding_a = strategy_a[64:70]
     encoding_b = strategy_b[64:70]
-    for i in range (0, 100):
+    for _ in range (0, 100):
         move_a = strategy_a[get_strategy(encoding_a)]
         move_b = strategy_b[get_strategy(encoding_b)]
         a, b = assign_score(move_a, move_b)
@@ -42,32 +43,31 @@ def play_game(strategy_a, strategy_b):
     return (score_a, score_b)    
                 
 
-def fitness_calculation(strategies, fitness):
-    """each strategy plays with all other 19 strategies 
-       and with itself i.e. a total of 20 games"""
+def fitness_calculation(strategies, fitness, num_organisms):
+    """each strategy plays with all other (num_organisms-1) strategies 
+       and with itself i.e. a total of num_organisms games"""
 
-    for i in range(0, 20):
-        for j in range(i, 20):
+    for i in range(0, num_organisms):
+        for j in range(i, num_organisms):
             x, y = play_game(strategies[i], strategies[j])
             fitness[i]+=x
             fitness[j]+=y
-    #averaging the fitness values over 20 strategies            
-    fitness = [f/20 for f in fitness]
+    #averaging the fitness values over num_organisms strategies            
+    fitness = [f/num_organisms for f in fitness]
     return fitness
 
-def fitness_proportionate_selection(strategies, fitness):
+def fitness_proportionate_selection(strategies, fitness, num_organisms):
     """choosing parent strategies on the basis of roulette 
        wheel selection, and we perform this process for 'n'
        number of times, where 
-       n --> number of strategies, here 20."""
+       n --> number of strategies, here num_organisms."""
 
     max_val = sum(fitness)
     parents = []
-    size = len(fitness)
-    for i in range(size):
+    for _ in range(num_organisms):
         temp_sum = 0
         rand_val = random.uniform(0, max_val)
-        for j in range(size):
+        for j in range(num_organisms):
             temp_sum += fitness[j]
             if temp_sum>=rand_val:
                 parents+=[strategies[j]]
@@ -90,32 +90,37 @@ def crossover(parent_1, parent_2):
     size = len(parent_1)
 
     #choosing a crossover point
-    crossover_point = random.randint(0, size-1)
-    if crossover_point==0:
-        return [parent_2, parent_1]
-    elif crossover_point==size-1:
-        return [parent_1, parent_2]
-    else:    
-        offspring_1 = parent_1[:crossover_point]+parent_2[crossover_point:]
-        offspring_2 = parent_2[:crossover_point]+parent_1[crossover_point:]
-    return [offspring_1, offspring_2]   
+    crossover_point = random.randint(0, size)
+    offspring_1 = parent_1[:crossover_point]+parent_2[crossover_point:]
+    offspring_2 = parent_2[:crossover_point]+parent_1[crossover_point:]
+    return [offspring_1, offspring_2]
 
-def evolve():
+def evolve(number_of_organisms, generations):
     """main function to facilitate all the evolution functionalities"""
 
     #crossover and mutation probabilities
     pc, pm = 0.7, 0.001
-    strategies = [(''.join(str(random.randint(0,1)) for _ in range(70))) for _ in range(20)]
-    generations = 10
+    strategies = [(''.join(str(random.randint(0,1)) for _ in range(70))) for _ in range(number_of_organisms)]
+
+    #temp list to keep a track of maximum fitness as the generations proceed
+    max_fitness_over_generations = []
 
     #running for certain number of generations
-    for _ in range(generations):
-        fitness = [0]*20
-        fitness = fitness_calculation(strategies, fitness)
-        parents = fitness_proportionate_selection(strategies, fitness)
-
+    for gen in range(generations):
+        print ('running for generation ', gen)
+        fitness = [0]*number_of_organisms
+        fitness = fitness_calculation(strategies, fitness, number_of_organisms)
+        parents = fitness_proportionate_selection(strategies, fitness, number_of_organisms)
+        
+        temp_max_fitness = max(fitness)
+        max_fitness_over_generations += [temp_max_fitness]
+        #displaying results
+        print ('Listing the strategies followed by the population for generation number ', gen)
+        for i in range(number_of_organisms):
+            print ('Stategy followed by organism number %d is %s'%(i, strategies[i]))
+        
         strategies.clear()
-        for j in range(0, 20, 2):
+        for j in range(0, number_of_organisms, 2):
             #crossover between parents
             if random.random()<=pc:
                 strategies+=crossover(parents[j], parents[j+1])
@@ -124,6 +129,15 @@ def evolve():
             
             #mutation in the generated offsprings.
             strategies[-2] = mutate(strategies[-2], pm)
-            strategies[-1] = mutate(strategies[-1], pm)    
+            strategies[-1] = mutate(strategies[-1], pm)
+    return max_fitness_over_generations            
 
-evolve()
+
+if len(sys.argv)<3:
+    print ('Usage: python3 evolution.py <number_of_organisms> <number_of_generations>')
+    sys.exit()
+num_organisms = int(sys.argv[1])
+num_generations = int(sys.argv[2])
+fitness_over_generations = evolve(num_organisms, num_generations)
+print ('\n The fitnesses over generations are as follows')
+print (fitness_over_generations)
